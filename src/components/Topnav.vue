d1f5b04739188ea43123b518a0d5eff055725d7c
mumu111111 <2646709035@qq.com>
2021-02-19 15:17:49 +0800

    整体布局样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -28,7 +28,10 @@ export default {
   background: pink;
   display: flex;
   padding: 16px;
-  position: relative;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
   z-index: 10; //不被挡住
   > .logo {
     max-width: 6em;
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,7 +1,7 @@
 
 <template>
-  <div>
-    <Topnav />
+  <div class="layout">
+    <Topnav class="nav" />
     <div class="content">
       <aside v-if="asideVisible">
         <h2>组件列表</h2>
@@ -42,10 +42,42 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
+    }
+  }
+}
+.content {
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
 aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
   > h2 {
     margin-bottom: 4px;
   }
@@ -53,6 +85,9 @@ aside {
     > li {
       padding: 4px 0;
     }
+  }
+  main {
+    overflow: auto;
   }
   /* 手机上才绝对定位 */
   @media (max-width: 500px) {
