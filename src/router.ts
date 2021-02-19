621a7fcb1af032b60761a890e3d50dcc0d746c39
mumu111111 <2646709035@qq.com>
2021-02-19 15:22:53 +0800

    框架基本完成

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,7 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
-
+import DocDemo from './components/DocDemo.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
@@ -16,6 +16,7 @@ const router = createRouter({
             path: '/doc',
             component: Doc,
             children: [
+                { path: "", component: DocDemo },
                 { path: "switch", component: SwitchDemo },
                 { path: "button", component: ButtonDemo },
                 { path: "dialog", component: DialogDemo },
