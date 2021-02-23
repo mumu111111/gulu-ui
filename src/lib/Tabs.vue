ed99dc2f9634c03357182d331b4542419ed94b80
mumu111111 <2646709035@qq.com>
2021-02-23 17:06:01 +0800

    渲染tab内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,4 +1,25 @@
 <template>
-  <!-- <div>tabs组件</div> -->
-  <slot />
+  <div>
+    tabs组件
+    <component :is="defaults[0]"></component>
+    <component :is="defaults[1]"></component>
+  </div>
 </template>
+<script>
+import Tab from "./Tab.vue";
+export default {
+  setup(props, context) {
+    console.log(context);
+    const defaults = context.slots.default();
+    console.log(defaults);
+    defaults.forEach((tag) => {
+      console.log(tag.type);
+      //   检查子组件类型
+      if (tag.type !== Tab) {
+        throw new Error("Tabs 子标签必须时 Tab");
+      }
+    });
+    return { defaults };
+  },
+};
+</script>
