a3f39ec5d068832b8365f6b25ac1b72769643fad
mumu111111 <2646709035@qq.com>
2021-02-24 18:22:13 +0800

    tab下划线css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -10,6 +10,7 @@
       >
         {{ t }}
       </div>
+      <div class="gulu-tabs-nav-indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <component
@@ -42,7 +43,6 @@ export default {
       return tag.props.title;
     });
     const select = (t) => {
-      console.log("desdsd");
       context.emit("update:selected", t);
     };
     return { defaults, titles, select };
@@ -58,6 +58,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -68,6 +69,14 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
     }
   }
   &-content {
