c16ea6b28f952ee62ec89dd41c0ded8b3409d294
mumu111111 <2646709035@qq.com>
2021-02-25 16:02:10 +0800

    下划线滑动效果

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="gulu-tabs">
-    <div class="gulu-tabs-nav">
+    <div class="gulu-tabs-nav" ref="container">
       <div
         class="gulu-tabs-nav-item"
         v-for="(t, index) in titles"
+        :ref="
+          (el) => {
+            if (el) navItems[index] = el;
+          }
+        "
         :key="index"
         :class="{ selected: t == selected }"
         @click="select(t)"
       >
         {{ t }}
       </div>
-      <div class="gulu-tabs-nav-indicator"></div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <component
@@ -23,7 +28,8 @@
     </div>
   </div>
 </template>
-<script>
+<script lang="ts">
+import { onMounted, onUpdated, ref } from "vue";
 import Tab from "./Tab.vue";
 export default {
   props: {
@@ -32,6 +38,24 @@ export default {
     },
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.filter((div) =>
+        div.classList.contains("selected")
+      )[0]; //div
+      const { width } = result.getBoundingClientRect(); // 导航的宽度
+      indicator.value.style.width = width + "px"; // 动态获取width
+
+      const { left: left1 } = container.value.getBoundingClientRect();
+      const { left: left2 } = result.getBoundingClientRect();
+      const left = left2 - left1;
+      indicator.value.style.left = left + "px";
+    };
+    onMounted(x);
+    onUpdated(x);
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       //   检查子组件类型
@@ -45,7 +69,7 @@ export default {
     const select = (t) => {
       context.emit("update:selected", t);
     };
-    return { defaults, titles, select };
+    return { defaults, titles, select, navItems, indicator, container };
   },
 };
 </script>
@@ -77,6 +101,7 @@ $border-color: #d9d9d9;
       left: 0;
       bottom: -1px;
       width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
