7309d3e4cece875f39702658628765ffe5739f93
mumu111111 <2646709035@qq.com>
2021-03-03 14:34:44 +0800

    从iconfont引入突变

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="topnav">
-    <div class="logo">LOGO</div>
-    <div class="menu"></div>
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-king"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -24,7 +27,9 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$color: #007974;
 .topnav {
+  color: $color;
   background: pink;
   display: flex;
   padding: 16px;
@@ -36,6 +41,10 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    > svg {
+      width: 32px;
+      height: 32px;
+    }
   }
   > .menu {
     display: flex;
