3f381fb2288842c33d57fc4ccfc615e532c9be97
mumu111111 <2646709035@qq.com>
2021-03-15 15:57:48 +0800

    为gulu-设定样式

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import "./lib/gulu.scss";
+import './index.scss';
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.scss'
 import { router } from './router'
 import './assets/svg.js'
 import 'github-markdown-css'
