73df58eef0002475f80d81d6494a31a8abc28720
mumu111111 <2646709035@qq.com>
2021-03-25 17:37:37 +0800

    创建dialog2

---
+++ src/components/Dialog2.demo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Button @click="showDialog">打开对话框</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+import { ref, h } from "vue";
+import { openDialog } from "../lib/Dialog.vue";
+export default {
+  components: {
+    Button,
+  },
+  setup() {
+    const showDialog = () => {
+      openDialog({
+        title: h("strong", {}, "标题"),
+        content: "你好",
+        ok() {
+          console.log("ok");
+        },
+        cancel() {
+          console.log("cancel");
+        },
+      });
+    };
+    return {
+      showDialog,
+    };
+  },
+};
+</script>
