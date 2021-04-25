18c8b472e4276d9745d28a269ba05a39da1d1ddc
mumu111111 <2646709035@qq.com>
2021-04-25 19:38:31 +0800

    修改名称

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="markdown-body">
     <h2>介绍</h2>
-    <p>V UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+    <p>COKE-UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
     <p>项目是为了学习 Vue 3而写的，尽量不采用第三方库。</p>
     <p>未发布到npm上。</p>
     <h2>源码</h2>
